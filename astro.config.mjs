9c01f13b323911ee4540a2968309b129635e02f0
Safe Vuln <safevuln@gmail.com>
2025-03-11 16:21:28 +0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
+import tailwind from '@astrojs/tailwind';
+import { sanityConfig } from './src/utils/sanity-client';
+
+// https://astro.build/config
+export default defineConfig({
+    image: {
+        domains: ['cdn.sanity.io']
+    },
+    integrations: [
+        sanity(sanityConfig),
+        tailwind({
+            applyBaseStyles: false
+        })
+    ],
+    vite: {
+        server: {
+            hmr: { path: '/vite-hmr/' },
+            allowedHosts: ['.netlify.app']
+        }
+    },
+    server: {
+        port: 3000
+    }
+});
